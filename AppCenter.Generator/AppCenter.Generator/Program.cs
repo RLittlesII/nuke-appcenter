174b0ee82bbfae8fb811420ead563e400d542298
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2020-10-27 00:55:05 -0500

    init: add initial solution
    Co-authored-by: David Driscoll <david.driscoll@gmail.com>

---
+++ AppCenter.Generator/AppCenter.Generator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.OpenApi;
+using Microsoft.OpenApi.Extensions;
+using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi.Readers;
+
+namespace AppCenter.Generator
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://raw.githubusercontent.com/microsoft/appcenter-cli/")
+            };
+
+            var stream = await httpClient.GetStreamAsync("master/swagger/bifrost.swagger.json");
+
+// Read V3 as YAML
+            var openApiDocument = new OpenApiStreamReader().Read(stream, out var diagnostic);
+
+// Write V2 as JSON
+            var outputString = openApiDocument.Serialize(OpenApiSpecVersion.OpenApi2_0, OpenApiFormat.Json);
+
+            Console.Write(outputString);
+        }
+    }
+    
+}
---
+++ index.ts
@@ -0,0 +1,25 @@
+import 'reflect-metadata'
+import { readdir, writeFileSync } from 'fs';
+import { basename, dirname, extname } from 'path';
+import { sync } from 'glob';
+import { from } from 'ix/iterable'
+import { map, filter } from 'ix/iterable/operators'
+const commandline = require('./node_modules/appcenter-cli/dist/util/commandline/option-decorators');
+const nukeSpec: any = {};
+
+// const commands = sync('./node_modules/appcenter-cli/dist/commands/**/*.js', { absolute: true });
+const commands = sync('./node_modules/appcenter-cli/dist/commands/codepush/release.js', { absolute: true });
+
+for (const { path, command } of from(commands).pipe(map(path => ({ path, module: require(path) })), filter(z => !!z.module.default), map(({ module, path }) => ({ path, command: module.default })))) {
+    const p = path.split('commands');
+    const commandPath = p[p.length-1].substring(1).split('/').map(z => basename(z , '.js'));
+    console.log(commandPath);
+    // const commandLine = 
+    console.log(commandline.getClassHelpText(command));
+    console.log(commandline.getOptionsDescription(command.prototype));
+    // console.log(command, Reflect.getPrototypeOf(command), Reflect.getMetadataKeys(Reflect.getPrototypeOf(command)));
+}
+
+writeFileSync('./nuke.spec.json', JSON.stringify(nukeSpec));
+
+// console.log(commands[0]);
